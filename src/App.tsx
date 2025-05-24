2aae2dcb80e24ce0ae39649c577b249dbe9952be
Vivek Singh <viveksingh9301@gmail.com>
2025-05-24 20:59:02 +0530

    feat: implement routing for WelcomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
 function App() {
   return (
     <div className="w-full">
-      <WelcomePage />
+      <Routes>
+        <Route path="/" Component={WelcomePage} />
+      </Routes>
     </div>
   );
 }
