509496df47de91b07ca4fd0c369eff33b966e5e1
Vivek Singh <viveksingh9301@gmail.com>
2025-05-26 23:07:29 +0530

    feat: update Home component to display Stark Industries logo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,15 @@
+import logo from "../../assets/img/stark-industries-logo.png";
 import "./Home.css";
 
 function Home() {
-  return <div className="home-container"></div>;
+  return (
+    <>
+      <div>
+        <img src={logo} alt="Stark Industries Logo" className="si-logo" />
+      </div>
+      <div className="home-container"></div>;
+    </>
+  );
 }
 
 export default Home;
