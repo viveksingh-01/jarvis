ccb6f127fff8cf9a72726505820d232d020b3846
Vivek Singh <viveksingh9301@gmail.com>
2025-05-27 19:03:03 +0530

    feat: add power button with Font Awesome icon to Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from "../../assets/img/stark-industries-logo.png";
 import "./Home.css";
 
@@ -7,7 +9,16 @@ function Home() {
       <div className="container-si-logo">
         <img src={logo} alt="Stark Industries Logo" className="si-logo" />
       </div>
-      <div className="home-container"></div>;
+      <div className="home-container">
+        <div className="container-power-btn">
+          <FontAwesomeIcon
+            icon={faPowerOff}
+            size={"4x"}
+            className="power-btn"
+          />
+        </div>
+      </div>
+      ;
     </>
   );
 }
