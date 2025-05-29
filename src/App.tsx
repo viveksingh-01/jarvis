7a4d46f3a54414d116311ae6f2437073157c6436
Vivek Singh <viveksingh9301@gmail.com>
2025-05-29 21:20:21 +0530

    feat: refactor routing structure to display SI logo on every component except Welcome

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
+import logo from "./assets/img/stark-industries-logo.png";
 import Home from "./pages/Home/Home";
 import Interface from "./pages/Interface/Interface";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
+
+function WithLogoLayout() {
+  return (
+    <>
+      <div className="container-si-logo">
+        <img src={logo} alt="Stark Industries Logo" className="si-logo" />
+      </div>
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="w-full">
       <Routes>
         <Route path="/" Component={WelcomePage} />
-        <Route path="/home" Component={Home} />
-        <Route path="/on" Component={Interface} />
+        <Route element={<WithLogoLayout />}>
+          <Route path="/home" Component={Home} />
+          <Route path="/on" Component={Interface} />
+        </Route>
       </Routes>
     </div>
   );
