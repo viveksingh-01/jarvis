f8d4b1deb12fe39fb3bd153af4db5b89d1999a6a
Vivek Singh <viveksingh9301@gmail.com>
2025-05-30 21:44:31 +0530

    feat: implement SpeechRecognition setup in Interface component

--- src/pages/Interface/Interface.tsx
+++ src/pages/Interface/Interface.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import "./Interface.css";
 
 function Interface() {
+  useEffect(() => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.lang = "en-US";
+    recognition.interimResults = true;
+  }, []);
+
   return <div className="container-interface jarvis-powered-off"></div>;
 }
 
