70ea4e04de166415f656de57b62865e30420de9d
Vivek Singh <viveksingh9301@gmail.com>
2025-05-31 13:22:16 +0530

    feat: integrate SpeechRecognition setup and listening functionality in Interface component

--- src/pages/Interface/Interface.tsx
+++ src/pages/Interface/Interface.tsx
@@ -1,9 +1,11 @@
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./Interface.css";
 
 function Interface() {
+  const recognitionRef = useRef<SpeechRecognition | null>(null);
+
   useEffect(() => {
     const SpeechRecognition =
       window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -11,11 +13,21 @@ function Interface() {
     const recognition = new SpeechRecognition();
     recognition.lang = "en-US";
     recognition.interimResults = true;
+
+    recognitionRef.current = recognition;
   }, []);
+
+  const startListening = () => {
+    recognitionRef.current?.start();
+  };
 
   return (
     <div className="container-interface jarvis-powered-off">
-      <div id="record-btn" className="record-btn-container">
+      <div
+        id="record-btn"
+        className="record-btn-container"
+        onClick={startListening}
+      >
         <FontAwesomeIcon
           icon={faMicrophone}
           size={"4x"}
