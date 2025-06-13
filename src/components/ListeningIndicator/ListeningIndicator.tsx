d0329604c7cb9abe4bad3119f46a35713fd5df35
Vivek Singh <viveksingh9301@gmail.com>
2025-06-13 18:39:15 +0530

    feat: add loading spinner to ListeningIndicator for enhanced visual feedback

--- src/components/ListeningIndicator/ListeningIndicator.tsx
+++ src/components/ListeningIndicator/ListeningIndicator.tsx
@@ -1,8 +1,10 @@
+import ScaleLoader from "react-spinners/ScaleLoader";
 import "./ListeningIndicator.css";
 
 function ListeningComponent({ isListening }: { isListening: boolean }) {
   return isListening ? (
     <div className="listening-indicator">
+      <ScaleLoader height={36} width={9} radius={50} margin={5} color={"#f00"} />
       <h3 className="ml-2" style={{ color: "#ccc" }}>
         Listening...
       </h3>
