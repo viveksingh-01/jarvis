f2805c99a7ca0d1d02285be8a904ecb26b106317
Vivek Singh <viveksingh9301@gmail.com>
2025-09-19 21:54:15 +0530

    feat: update ListeningComponent to display speech text when available

--- src/components/ListeningIndicator/ListeningIndicator.tsx
+++ src/components/ListeningIndicator/ListeningIndicator.tsx
@@ -1,12 +1,12 @@
 import ScaleLoader from "react-spinners/ScaleLoader";
 import "./ListeningIndicator.css";
 
-function ListeningComponent({ isListening }: { isListening: boolean }) {
+function ListeningComponent({ isListening, speech }: { isListening: boolean; speech: string }) {
   return isListening ? (
     <div className="listening-indicator">
       <ScaleLoader height={36} width={9} radius={50} margin={5} color={"#f00"} />
       <h3 className="ml-2" style={{ color: "#ccc" }}>
-        Listening...
+        {speech?.length > 0 ? speech : "Listening..."}
       </h3>
     </div>
   ) : null;
